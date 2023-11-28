fedcc30f7ee212ec7bdb01ef8f9c2f7a8b0c9d8f
MDMAenjoyerr <vikaonelove4me@gmail.com>
2023-11-28 15:42:11 +0100

    Update main.py
    add: lizard
    add: spock
    add: score

--- main.py
+++ main.py
@@ -1,45 +1,42 @@
 import random
 
-def get_user_choice():
-    user_choice = input("Выберите: камень, ножницы или бумага? ").lower()
-    while user_choice not in ['камень', 'ножницы', 'бумага']:
-        print("Неверный ввод. Пожалуйста, выберите камень, ножницы или бумагу.")
-        user_choice = input("Выберите: камень, ножницы или бумага? ").lower()
-    return user_choice
-
-
-def get_computer_choice():
-    return random.choice(['камень', 'ножницы', 'бумага'])
-
-def determine_winner(user_choice, computer_choice):
-    if user_choice == computer_choice:
-        return "Ничья!"
-    elif (
-            (user_choice == 'камень' and computer_choice == 'ножницы') or
-            (user_choice == 'ножницы' and computer_choice == 'бумага') or
-            (user_choice == 'бумага' and computer_choice == 'камень')
-    ):
-        return f"Вы выиграли!"
-    else:
-        return f"Вы проиграли."
-
-
 def game():
-    print("Добро пожаловать в игру 'камень, ножницы, бумага'!")
+    choices = ["rock", "scissors", "paper", "lizard", "Spock"]
 
-    while True:
-        user_choice = get_user_choice()
-        computer_choice = get_computer_choice()
+    computer_choice = random.choice(choices)
 
-        print(f"Вы выбрали: {user_choice}")
-        print(f"Компьютер выбрал: {computer_choice}")
+    print("Choose your option:")
+    for i, choice in enumerate(choices, 1):
+        print(f"{i}. {choice}")
 
-        result = determine_winner(user_choice, computer_choice)
-        print(result)
+    player_choice_index = int(input()) - 1
+    player_choice = choices[player_choice_index]
 
-        play_again = input("Хотите сыграть еще раз? (да/нет) ").lower()
-        if play_again != 'да':
-            print("Спасибо за игру! До свидания.")
-            break
+    print(f"You chose: {player_choice}")
+    print(f"Computer chose: {computer_choice}")
 
-game()
+    if player_choice == computer_choice:
+        print("It's a tie!")
+        return 0
+    elif (
+        (player_choice == "rock" and (computer_choice == "scissors" or computer_choice == "lizard")) or
+        (player_choice == "scissors" and (computer_choice == "paper" or computer_choice == "lizard")) or
+        (player_choice == "paper" and (computer_choice == "rock" or computer_choice == "Spock")) or
+        (player_choice == "lizard" and (computer_choice == "paper" or computer_choice == "Spock")) or
+        (player_choice == "Spock" and (computer_choice == "rock" or computer_choice == "scissors"))
+    ):
+        print("You win!")
+        return 1
+    else:
+        print("You lose!")
+        return -1
+
+score = 0
+question = "yes"
+while question.lower() == "yes":
+    result = game()
+    score += result
+    print(f"Your current score: {score}")
+    question = input("Do you want to play again? (yes/no): ")
+
+print("Thanks for playing! Your final score:", score)
